553834a72afa5668b2cd7560250387cb6c8947e9
mike zhai <mzhai2@emory.edu>
2015-11-22 10:23:30 -0500

    more stats

--- data_statistics.py
+++ data_statistics.py
@@ -22,3 +22,25 @@ if __name__ == "__main__":
             q_with_answer += 1
 
     print("For train, 1 answer: %d, many answers: %d" % (q_with_answer, q_with_answers))
+
+answerCounts = {0: 1245, 1: 745, 2: 103, 3: 20, 7: 2, 4: 1, 5: 1, 6: 1}
+sum = 0
+for k,v in answerCounts.iteritems():
+    sum += v
+
+pmf = []
+
+for k,v in answerCounts.iteritems():
+    pmf.append(float(v)/sum)
+cdf = []
+for i,v in enumerate(pmf):
+    sum = 0
+    for j in range(0,i+1):
+        sum += pmf[j]
+    cdf.append(sum)
+
+print pmf
+print cdf
+
+#[0.5878186968838527, 0.35174693106704435, 0.04863078375826251, 0.009442870632672332, 0.00047214353163361664, 0.00047214353163361664, 0.00047214353163361664, 0.0009442870632672333]
+#[0.5878186968838527, 0.939565627950897, 0.9881964117091595, 0.9976392823418319, 0.9981114258734655, 0.9985835694050991, 0.9990557129367327, 0.9999999999999999]
